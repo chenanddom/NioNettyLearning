39a9e5501432bb97698889e9893afd1075bc9956
chend <chend@seirobotics.net>
2021-02-05 19:54:23 +0800

    提交netty实现http协议的部分代码

--- netty-http/src/main/java/com/itdom/HttpFileServer.java
+++ netty-http/src/main/java/com/itdom/HttpFileServer.java
@@ -1,13 +1,59 @@
 package com.itdom;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
 public class HttpFileServer {
 
-private static final String DEFAULT_URL="";
+    private static final String DEFAULT_URL = "";
 
+    public void run(final int port, final String url) {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>(){
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast("http-decoder",new HttpRequestDecoder());
+                            /**
+                             * HttpObjectAggregator解码器，它的作用是将多个消息转换伟单一的FullHttpRequest或者FullResponse,原因是uHTTP解码器在每个
+                             * HTTP消息中会生成多个i西澳西对象。
+                             * HttpRequest/HttpResponse
+                             * HttpContent
+                             * LastHttpContent.
+                             */
+                            socketChannel.pipeline().addLast("http-aggregator",new HttpObjectAggregator(65536));
+                            socketChannel.pipeline().addLast("http-encoder",new HttpResponseEncoder());
+                            /**
+                             *
+                             */
+                            socketChannel.pipeline().addLast("http-chunked",new ChunkedWriteHandler());
 
+                        }
+                    });
 
+            ChannelFuture future = bootstrap.bind("192.168.10.226",port).sync();
+            future.channel().closeFuture().sync();
 
-
-
-
+        }catch (Exception e){}finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+    public static void main(String[] args) {
+        new HttpFileServer().run(8088,DEFAULT_URL);
+    }
 }
